9665dc149e50618b6ed7d4557ac8aa29a12a0382
bilal elsayed <bilalelsayed97@gmail.com>
2024-02-06 22:57:37 +0200

    Logic Impelmintaion

--- xylophone/ViewController.swift
+++ xylophone/ViewController.swift
@@ -1,21 +1,35 @@
-//
-//  ViewController.swift
-//  xylophone
-//
-//  Created by bilal on 06/02/2024.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer?
+    var url: URL!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+     
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(input: sender.titleLabel?.text ?? "C")
     }
-    
-}
 
+    func playSound(input: String) {
+        if let soundURL = Bundle.main.url(forResource: "Sounds/\(input)", withExtension: "wav") {
+            url = soundURL
+        } else {
+            print("Error: Could not find the sound file.")
+        }
+        do {
+            guard let soundURL = url else {
+                print("Error: Sound URL is nil.")
+                return
+            }
+            player = try AVAudioPlayer(contentsOf: soundURL)
+            player?.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+}
